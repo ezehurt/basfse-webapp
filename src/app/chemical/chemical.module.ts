56231eddd1e5c120dd88c8464ae3ca507877f748
Ezequiel Hurt <ezehurt@gmail.com>
2020-12-30 23:24:04 +0100

    [BASFSE-001] Searchbar  basic scaffolding

--- src/app/chemical/chemical.module.ts
+++ src/app/chemical/chemical.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChemicalWindowComponent } from './chemical-window/chemical-window.component';
+import { ChemicalSearchBoxComponent } from './chemical-search-box/chemical-search-box.component';
+import { AngularMaterialModule } from '../angular-material.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    ChemicalWindowComponent
+    ChemicalWindowComponent,
+    ChemicalSearchBoxComponent
   ],
   exports: [
     ChemicalWindowComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AngularMaterialModule,
+    ReactiveFormsModule
   ]
 })
 export class ChemicalModule { }
